2cb40c3c721328091766d0e5c1a6c3e3669d82cc
SundaramPrince <106753437+Sundaramprince@users.noreply.github.com>
2025-05-17 22:16:00 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,19 @@
+const profile = {
+    username: 'shradhakhapra',
+    followers: 1000,
+    following: 500,
+    posts:195 ,
+    bio: 'I am a software engineer',
+    location: 'India',  
+};
+const posts = {
+    link1: 'https://www.instagram.com/p/Cr1g4c3rXlY/',
+    link1: 'https://www.instagram.com/sundaram_prince_kumar/?hl=en',
+
+
+
+};
+console.log(profile);
+console.log(posts);
+console.log(posts.link1);
+// const { username, followers, following, posts, bio, location } = profile;
